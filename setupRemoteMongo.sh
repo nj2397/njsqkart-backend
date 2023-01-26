8774179030bce872628c909b9bc9d6c05bab80b1
NIBJYOTI NATH <nibjyotin.m@yahoo.com>
2023-01-26 14:00:01 +0530

    Update setupRemoteMongo.sh

--- setupRemoteMongo.sh
+++ setupRemoteMongo.sh
@@ -1,3 +1,3 @@
 # Setup file template to upload data to MongoDB Atlas
-mongoimport --uri "mongodb+srv://njsqkart-node:#Mongodbnjn11@njsqkart-node.j9q58d3.mongodb.net/?retryWrites=true&w=majority" --drop --collection users --file data/export_qkart_users.json
-mongoimport --uri "mongodb+srv://njsqkart-node:#Mongodbnjn11@njsqkart-node.j9q58d3.mongodb.net/?retryWrites=true&w=majority" --drop --collection products --file data/export_qkart_products.json
+mongoimport --uri "mongodb+srv://njsqkart-node:%23Mongodbnjn11@njsqkart-node.j9q58d3.mongodb.net/?retryWrites=true&w=majority" --drop --collection users --file data/export_qkart_users.json
+mongoimport --uri "mongodb+srv://njsqkart-node:%23Mongodbnjn11@njsqkart-node.j9q58d3.mongodb.net/?retryWrites=true&w=majority" --drop --collection products --file data/export_qkart_products.json
