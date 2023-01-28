6a27d56525ae4c9aea3d1389c936be2ae23bfb56
NIBJYOTI NATH <nibjyotin.m@yahoo.com>
2023-01-28 22:39:14 +0530

    Update index.js

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -8,15 +8,13 @@ const cartRoute = require("./cart.route");
 
 const router = express.Router();
 
-// TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS - Reroute all API requests beginning with the `/v1/users` route to Express router in user.route.js
 
 router.use("/users", userRoute);
 
-
-// TODO: CRIO_TASK_MODULE_AUTH - Reroute all API requests beginning with the `/v1/auth` route to Express router in auth.route.js 
 router.use("/auth", authRoute);
 
 router.use("/products", productRoute);
+
 router.use("/cart", cartRoute);
 
 module.exports = router;
