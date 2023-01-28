25b92ba20986ce11ad7404bcdfdf4e93582e1228
NIBJYOTI NATH <nibjyotin.m@yahoo.com>
2023-01-28 22:38:34 +0530

    Update auth.route.js

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -6,7 +6,7 @@ const authController = require("../../controllers/auth.controller");
 
 const router = express.Router();
 
-// TODO: CRIO_TASK_MODULE_AUTH - Implement "/v1/auth/register" and "/v1/auth/login" routes with request validation
+
 router.post("/register", validate(authValidation.register), authController.register);
 router.post("/login", validate(authValidation.login), authController.login);
 
