3c6fa3002e1bbf74ba19536bbdb748c91c115c0d
NIBJYOTI NATH <nibjyotin.m@yahoo.com>
2023-01-28 22:48:45 +0530

    Update auth.validation.js

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -1,7 +1,6 @@
 const Joi = require("joi");
 const { password } = require("./custom.validation");
 
-// TODO: CRIO_TASK_MODULE_AUTH - Define request validation schema for user registration
 /**
  * Check request *body* for fields (all are *required*)
  * - "email" : string and satisyfing email structure
