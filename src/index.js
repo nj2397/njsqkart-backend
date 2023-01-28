1f6bcaca54347207b156c5d51fffd29eb700431c
NIBJYOTI NATH <nibjyotin.m@yahoo.com>
2023-01-28 22:49:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ const config = require("./config/config");
 let server;
 const port = config.port;
 
-// TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS - Create Mongo connection and get the express app to listen on config.port
-
 mongoose 
 .connect(config.mongoose.url, config.mongoose.options)
 .then(() => {
